66e777d0a1594a1d6aa432da283ff8780d6c1d71
BlaneTech <falloud70gmail.com>
2022-03-14 19:36:05 +0000

    modification du code aprés modif de la db, le lundi 14-03

--- bd.py
+++ bd.py
@@ -15,22 +15,34 @@ except Error:
     print("Erreur lors de la connection à MYSQL")
 curseur = dbConnect.cursor()
 list_id_devoir= list()
-list_id_matiere = list()
-etudiant = "INSERT INTO ETUDIANT (numero_ETUDIANT, nom_ETUDIANT, prenom_ETUDIANT, dateNais_ETUDIANT) VALUES (%s,%s,%s,%s)"
-devoir = "INSERT INTO DEVOIR(numero_DEVOIR, note_DEVOIR) VALUES (%s,%s)"
-classe = "INSERT INTO CLASSE (numero_ETUDIANT, nom_CLASSE) VALUES (%s,%s)"
-matiere = "INSERT INTO MATIERE (nom_MATIERE,id_DEVOIR) VALUES (%s,%s)"
+list_nomClassse = list()
+etudiant = "INSERT INTO ETUDIANT (numero_ETUDIANT, nom_ETUDIANT, prenom_ETUDIANT, dateNais_ETUDIANT,nom_CLASSE) VALUES (%s,%s,%s,%s,%s)"
+devoir = "INSERT INTO DEVOIR(numero_DEVOIR, note_DEVOIR,nom_MATIERE) VALUES (%s,%s,%s)"
+classe = "INSERT INTO CLASSE (nom_CLASSE) VALUES (%s)"
+matiere = "INSERT INTO MATIERE (nom_MATIERE) VALUES (%s)"
 faire = "INSERT INTO FAIRE (numero_ETUDIANT, id_DEVOIR) VALUES (%s,%s)"
-suivre = "INSERT INTO SUIVRE (numero_ETUDIANT, id_MATIERE,noteExamen_CONCERNER) VALUES (%s,%s,%s)"
+suivre = "INSERT INTO SUIVRE (numero_ETUDIANT, nom_MATIERE,noteExamen_CONCERNER) VALUES (%s,%s,%s)"
+
+# insertion dans matiere
+for key in dico[1]['note'].keys():
+    curseur.execute(matiere,(key,))
+
+#insertion dans la table  classe
 for i in range(1,len(dico)):
-    curseur.execute(etudiant, (dico[i]['numero'],dico[i]['nom'],dico[i]['prenom'],dico[i]['date']))
-
-    curseur.execute(classe,(dico[i]['numero'],dico[i]['classe']))
-
+    if dico[i]['classe'] not in list_nomClassse:
+            list_nomClassse.append(dico[i]['classe'])
+for name in range(len(list_nomClassse)):
+    curseur.execute(classe,(list_nomClassse[name],))
+# insertion dans etudiant
+for i in range(1,len(dico)):
+    curseur.execute(etudiant, (dico[i]['numero'],dico[i]['nom'],dico[i]['prenom'],dico[i]['date'],dico[i]['classe']))
+    
+    # insertion  dans la table devoir
     for key in dico[i]['note'].keys():
         for j in range(len(dico[i]['note'][key]['devoir'])):
-            curseur.execute(devoir,(j+1,dico[i]['note'][key]['devoir'][j],))
+            curseur.execute(devoir,(j+1,dico[i]['note'][key]['devoir'][j],key))
 
+#insertion dans la table faire
 curseur.execute("SELECT id_DEVOIR FROM DEVOIR")
 for idDevoir in curseur.fetchall():
     list_id_devoir.append(idDevoir)
@@ -38,20 +50,13 @@ num_id = 0
 for i in range(1,len(dico)):
     for key1 in dico[i]['note'].keys():
         for k in range(len(dico[i]['note'][key1]['devoir'])):
-            curseur.execute(matiere,(key1,list_id_devoir[num_id][0]))
             curseur.execute(faire,(dico[i]['numero'],list_id_devoir[num_id][0]))
             num_id+=1
     
-curseur.execute("SELECT id_MATIERE FROM MATIERE")
-for idMatiere in curseur.fetchall():
-    list_id_matiere.append(idMatiere)
-    list_id_matiere.sort()
-num_idm = 0
-
 for i in range(1,len(dico)):
     for key2 in dico[i]['note'].keys():
-        curseur.execute(suivre,(dico[i]['numero'],list_id_matiere[num_idm][0],dico[i]['note'][key2]['examen']))
-        num_idm += len(dico[i]['note'][key2]['devoir'])
+        curseur.execute(suivre,(dico[i]['numero'],key2,dico[i]['note'][key2]['examen']))
+        # num_idm += len(dico[i]['note'][key2]['devoir'])
 
 dbConnect.commit()
 dbConnect.close()
